1d85751791f5b2e6905c7ed92f9ef87cee521b43
NELSON <nelson@MacBook-Air-NELSON.local>
2019-08-19 17:39:56 +0200

    Update

---
+++ src/components/Claim.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="wrapper">
+    <h1 class="claim"> SPACER</h1>
+    <p class="subclaim">
+        Begin yout jurney ...
+    </p>
+    <p class="subclaim">
+        Type anything space-related to start.
+    </p>
+</div>
+</template>
+<script>
+export default {
+    name: 'Claim',
+};
+</script>
+
+<style lang="scss" scoped>
+    .wrapper{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+</style>
